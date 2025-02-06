97939422d783e2e3ce58e6d6942a6d3d78060679
Tyler Wade <vaxonreal@gmail.com>
2025-02-06 13:48:58 -0500

    Added API_URL const to use from env variable when available.

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const API_URL = process.env.API_URL || "http://localhost:8080/api";
+
 const instance = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: API_URL,
   withCredentials: true
 });
 
